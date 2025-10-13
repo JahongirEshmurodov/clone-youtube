ff4a8aade4349cbd2b7c6a8549d89dc9a97dcd1e
JahongirEshmurodov <johonbek0808@gmail.com>
2025-10-13 23:07:42 +0500

    ds

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -13,6 +13,7 @@ function Videos({ videos }) {
       width={"100%"}
       direction={"row"}
       flexWrap={"wrap"}
+      s
       justifyContent={"center"}
       alignItems={"center"}
       gap={2}
